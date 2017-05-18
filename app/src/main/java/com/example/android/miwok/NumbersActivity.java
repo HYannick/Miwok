7dac41f85b3713a241ec0a288f1474c14eb4f3d5
yhoussin <yhoussin@sqli.com>
2017-05-18 17:49:42 +0200

    test gridview

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,10 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -23,24 +26,8 @@ public class NumbersActivity extends AppCompatActivity {
 
         ArrayList<String> words = new ArrayList<String>(Arrays.asList("one","two","three","four","five","six","seven","eight","nine","ten"));
 
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-        for(int i = 0; i < words.size(); i++) {
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(i));
-            rootView.addView(wordView);
-        }
-
-//        int i = 0;
-//        int arrayL = words.size();
-//
-//        while (i < arrayL) {
-//            TextView wordView = new TextView(this);
-//            wordView.setText(words.get(i));
-//            rootView.addView(wordView);
-//            i++;
-//        }
-
-
-
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        GridView listView = (GridView) findViewById(R.id.rootView);
+        listView.setAdapter(itemsAdapter);
     }
 }
